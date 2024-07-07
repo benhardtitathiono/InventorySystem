64c5c41ac90051b83a0e11051aeb3d19112f8352
benhardtitathiono <113670466+benhardtitathiono@users.noreply.github.com>
2024-07-07 14:25:11 +0700

    tambah tabel barang pinjam

---
+++ app/Models/BarangPinjam.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BarangPinjam extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_06_23_122445_create_table_borrowed.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableBorrowed extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('identitas_peminjam_id')->required();
+            $table->foreign('identitas_peminjam_id')->references('id')->on('identitas_peminjam');
+
+            $table->date('tanggal_pinjam');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+}

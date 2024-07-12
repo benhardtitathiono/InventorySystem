1fbd433a88972e3fd1eead1e22452ffdaa9a58ee
benhardtitathiono <113670466+benhardtitathiono@users.noreply.github.com>
2024-07-12 09:24:20 +0700

    update view peminjaman barang

--- resources/views/peminjaman/index.blade.php
+++ resources/views/peminjaman/index.blade.php
@@ -16,66 +16,61 @@
             <div class="alert alert-success">{{ session('message') }}</div>
         @endif
         <div style="margin-bottom: 10px">
-            {{-- <a href="category/create" class="btn btn-success">Add New Data</a> --}}
-            <a href="#modalCreate" data-toggle="modal" class="btn btn-info">Tambah produk baru</a>
-            <a href="#modalEditProduct" data-toggle="modal" class="btn btn-info" onclick="getDeleteProductList()">Lihat Data
-                Yang Dihapus</a>
-
+            <a href="#modalCreate" data-toggle="modal" class="btn btn-info">Tambah Barang Baru</a>
+            <a href="#modalEditBarang" data-toggle="modal" class="btn btn-info"
+                onclick="getDeleteBarangList('@php echo $_GET['kategori'] @endphp')">Lihat Data Yang Dihapus</a>
+            <form method="POST" action="{{ route('productpinjam.getBorrowForm') . '?kategori=' . $_GET['kategori'] }}">
+                @csrf
+                <button type="submit" class="btn btn-info" id="borrowForm">Tambah Peminjaman Baru</button>
+            </form>
         </div>
 
         @if (session('status'))
             <div class="alert alert-success">{{ session('status') }}</div>
         @endif
 
-        <table id="listProduct" class="table">
+        <table id="listBarang" class="table">
             <thead>
                 <tr>
                     <th>ID</th>
-                    <th>Nama Produk</th>
-                    <th>Satuan</th>
+                    <th>Nama Barang</th>
                     <th>Deskripsi</th>
                     <th>Stok</th>
+                    <th>Stok Tersedia</th>
+                    <th>Tanggal Ditambahkan</th>
                     <th>Terakhir Diperbarui</th>
 
                     <th>Action</th>
                 </tr>
             </thead>
             <tbody>
-                @if (isset($pap))
-                    @foreach ($pap as $p)
+                @if (isset($barangPinjam) && count($barangPinjam) > 0)
+                    @foreach ($barangPinjam as $bp)
                         <tr>
-                            <td>{{ $p->id }}</td>
-                            <td class="editable" id="td-nama_barang-{{ $p->id }}">{{ $p->nama_barang }}</td>
-                            <td class="editable" id="td-satuan-{{ $p->id }}">{{ $p->satuan }}</td>
-                            <td class="editable" id="td-deskripsi-{{ $p->id }}">{{ $p->deskripsi }}</td>
-                            <td id="td-jumlah-{{ $p->id }}">{{ $p->jumlah }}</td>
-                            <td id="td-created_at-{{ $p->id }}">{{ $p->updated_at }}</td>
+                            <td>{{ $bp->id }}</td>
+                            <td class="editable" id="td-nama_barang-{{ $bp->id }}">{{ $bp->nama_barang }}</td>
+                            <td class="editable" id="td-deskripsi-{{ $bp->id }}">{{ $bp->deskripsi }}</td>
+                            <td id="td-jumlah-{{ $bp->id }}">{{ $bp->jumlah }}</td>
+                            <td>{{ $bp->jumlah - $bp->totalBarangDipinjam }}</td>
+                            <td id="td-created_at-{{ $bp->id }}">{{ $bp->created_at }}</td>
+                            <td id="td-updated_at-{{ $bp->id }}">{{ $bp->updated_at }}</td>
 
                             <td style="text-align: center">
-                                <a href="#modalEditProduct" data-toggle="modal" class="btn btn-warning btn-xs"
-                                    onclick="getUpdateStokForm({{ $p->id }})">Tambah Stok</a>
-                                <a href="#modalEditProduct" data-toggle="modal" class="btn btn-warning btn-xs"
-                                    onclick="getUpdateStokOutForm({{ $p->id }})">Kurang Stok</a>
-                                <a href="#modalEditProduct" data-toggle="modal" class="btn btn-warning btn-xs"
-                                    onclick="getLogProduct({{ $p->id }})">Log Stok</a>
-                                <form method="POST" action="{{ route('product.destroy', $p->id) }}">
+                                {{-- <a href="#modalEditBarang" data-toggle="modal" class="btn btn-warning btn-xs"
+                                    onclick="getBorrowForm({{ $bp->id }})">Peminjaman</a> --}}
+                                <form method="POST" action="{{ route('productpinjam.destroy', $bp->id) }}">
                                     @csrf
-                                    {{-- @method('DELETE') --}}
                                     <input type="submit" value="Hapus" class="btn btn-danger"
-                                        onclick="return confirm('Apakah yakin ingin menghapus produk dengan ID {{ $p->id }} - {{ $p->nama_barang }}?')">
+                                        onclick="return confirm('Apakah yakin ingin menghapus barang dengan ID {{ $bp->id }} - {{ $bp->nama_barang }}?')">
                                 </form>
                             </td>
                         </tr>
                     @endforeach
-                @else
-                    <td style="text-align: center; font-size:20px; font-weight:bold;" colspan="7">Tidak ada Produk
-                    </td>
                 @endif
-
             </tbody>
         </table>
 
-        <div class="modal fade" id="modalEditProduct" tabindex="-1" role="basic" aria-hidden="true">
+        <div class="modal fade" id="modalEditBarang" tabindex="-1" role="basic" aria-hidden="true">
             <div class="modal-dialog modal-wide">
                 <div class="modal-content">
                     <div class="modal-body" id="modalContent">
@@ -91,38 +86,36 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-                    <h4 class="modal-title">Tambah produk baru</h4>
+                    <h4 class="modal-title">Tambah barang baru</h4>
                 </div>
-                <div class="modal-body">
-                    <form method="POST" action="{{ route('product.store') }}" enctype="multipart/form-data">
+                <form id="formBorrow" method="POST" action="{{ route('productpinjam.store') }}"
+                    enctype="multipart/form-data">
+                    <div class="modal-body">
                         @csrf
                         <div class="form-group">
-                            <label for="nameProd">Nama Produk</label>
-                            <input type="text" class="form-control" name="nameProduct" id="nameProd"
-                                aria-describedby="nameHelp" placeholder="Nama produk">
+                            <label for="nameBarang">Nama Barang</label>
+                            <input type="text" class="form-control" name="nameBarang" id="nameBarang"
+                                aria-describedby="nameHelp" placeholder="Nama Barang">
 
-                            <label for="satuanProd">Satuan</label>
-                            <input type="text" class="form-control" name="satuanProd" id="satuanProd"
-                                aria-describedby="satuanHelp" placeholder="Satuan">
-
-                            <label for="descProd">Deskripsi</label>
-                            <input type="text" class="form-control" name="descProd" id="descProd"
+                            <label for="descBarang">Deskripsi</label>
+                            <input type="text" class="form-control" name="descBarang" id="descBarang"
                                 aria-describedby="descHelp" placeholder="Deskripsi">
 
-                            <label for="jumlahProd">Stok</label>
-                            <input type="number" class="form-control" name="jumlahProd" id="jumlahProd"
-                                aria-describedby="jumlahHelp" placeholder="Stok awal" min="0">
+                            <label for="jumlahBarang">Stok</label>
+                            <input type="number" class="form-control" name="jumlahBarang" id="jumlahBarang"
+                                aria-describedby="jumlahHelp" placeholder="Stok awal" min="1">
 
-                            <label for="tgglEx">Tanggal Kadaluwarsa</label>
-                            <input type="date" class="form-control" name="dateExProd" id="dateExProd"
-                                aria-describedby="dateExHelp"
-                                min="{{ \Carbon\Carbon::now('Asia/Jakarta')->format('Y-m-d') }}">
+                            <label for="tipeBarang">Stok</label>
+                            <select name="tipeBarang" id="tipeBarang" class="form-control" aria-describedby="tipeHelp">
+                                <option value="Poli Gigi">Poli Gigi</option>
+                                <option value="Umum">Umum</option>
+                            </select>
                         </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                    </form>
-                </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -131,65 +124,58 @@
 @section('javascript')
     <script>
         $(document).ready(function() {
-            $('#listProduct').DataTable({
+            $('#listBarang').DataTable({
                 responsive: true,
+
                 "pagingType": "full_numbers",
+
+                order: [
+                    [1, 'asc']
+                ],
+
                 lengthMenu: [
                     [5, 10, 25, 50, -1],
                     [5, 10, 25, 50, 'All']
                 ],
 
                 columnDefs: [{
+                    // data: null,
+                    // defaultContent: 'Tidak ada data barang2',
                     className: 'dtr-control',
-                    orderable: false,
-                    target: 0
+                    orderable: true,
+                    target: '_all'
                 }],
+
                 responsive: {
                     details: {
                         type: 'column',
                         target: 'tr'
                     }
                 },
+
+                language: {
+                    emptyTable: 'Tidak ada data barang',
+                    info: "Tampilan _START_ sampai _END_ dari _TOTAL_ data",
+                    infoEmpty: 'Tidak ada data barang',
+                    infoFiltered: "(Pencarian dari _MAX_ total data)",
+                    lengthMenu: "Tampilkan _MENU_ Data",
+                    zeroRecords: 'Tidak ada data barang'
+                },
             });
         });
 
-        // function getUpdateStokForm(id) {
-        //     $.ajax({
-        //         type: 'POST',
-        //         url: '{{ route('product.getUpdateStokForm') }}',
-        //         data: {
-        //             '_token': '<?php echo csrf_token(); ?>',
-        //             'id': id
-        //         },
-        //         success: function(data) {
-        //             $('#modalContent').html(data.msg)
-        //         }
-        //     });
-        // }
-
-        // function getUpdateStokOutForm(id) {
-        //     $.ajax({
-        //         type: 'POST',
-        //         url: '{{ route('product.getUpdateStokOutForm') }}',
-        //         data: {
-        //             '_token': '<?php echo csrf_token(); ?>',
-        //             'id': id
-        //         },
-        //         success: function(data) {
-        //             $('#modalContent').html(data.msg)
-        //         }
-        //     });
-        // }
-
-        // function getDeleteProductList() {
-        //     $.ajax({
-        //         type: 'GET',
-        //         url: '{{ route('product.getDeleteProductList') }}',
-        //         success: function(data) {
-        //             $('#modalContent').html(data.msg)
-        //         }
-        //     });
-        // }
+        function getDeleteBarangList(tipe) {
+            $.ajax({
+                type: 'POST',
+                url: "{{ url('productpinjamterhapus') }}/" + tipe,
+                data: {
+                    '_token': '<?php echo csrf_token(); ?>',
+                },
+                success: function(data) {
+                    $('#modalContent').html(data.msg)
+                }
+            });
+        }
 
         // function getLogProduct(id) {
         //     $.ajax({
